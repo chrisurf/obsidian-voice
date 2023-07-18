1f9bb6e08ad5a4f4394e2923865aafa54430d255
chris oguntolu <44543411+chrisurf@users.noreply.github.com>
2023-07-18 09:27:24 +0200

    🔊 The Obsidain Voice Plugin to listen to your notes aloud 🚀

---
+++ README.md
@@ -0,0 +1,33 @@
+# Obsidian Voice Plugin 🔊
+
+The Voice Plugin for Obsidian brings a new dimension to your note-taking experience by adding a voice feature to your Obsidian workspace. This plugin allows you to listen to your written content within Obsidian, offering a hands-free and convenient way to consume your notes and documents.
+
+Whether you want to listen to your documents while multitasking, reinforce your learning through auditory input, or simply enjoy a unique way of interacting with your content, the Voice Plugin delivers a seamless and customizable text-to-speech solution within the Obsidian environment.
+
+## Features
+
+-   **Text-to-Speech**: The Voice Plugin leverages advanced text-to-speech technology to synthesize natural-sounding human speech, enabling your written content in Obsidian to be read aloud.
+-   **Private Communication**: Enjoy a completely private communication experience as the Voice Plugin utilizes your personal AWS account, ensuring you have full control over your data.
+-   **Voice Control**: Play and pause the captivating narration at your fingertips, granting you the power to immerse yourself in your notes on your own terms.
+-   **Content Selection**: By default, the Voice Plugin intelligently plays the entire text content within the file. However, for a more tailored experience, simply select the specific sections you want to hear, giving you the freedom to focus on what truly matters.
+-   **Voice Options**: Choose from a selection of six available voices to customize the reading experience to your preference.
+-   **AWS Polly Support**: Benefit from the power of AWS Polly, an advanced text-to-speech service that offers high-quality and human like voices.
+-   **AWS Account Integration**: Use your own AWS account credentials to utilize AWS Polly with the Voice Plugin.
+-   **Localisation**: Choose your preferred AWS Region to optimize performance and ensure the best experience with the Voice Plugin.
+-   **Mobile Support**: Seamlessly integrated into the mobile Obsidian application, it ensures you can enjoy a similar audio experience wherever you are.
+-   **Caching**: Each speech is cached until new content will be submitted. Ensuring a seamless experience while keeping your expenses in check.
+-   **Hotkey**: Define your custom shortcuts for quickly starting the voice synthesizer.
+
+> **Note**: Just a heads up, when it comes to synthesizing large content files, give it a moment to work its magic. You'll know the Voice Plugin is hard at work when you see that slick Ribbon Icon doing its thing. Once the processing is completed, that Ribbon Icon will switch things up and show off a cool pause icon. So sit back, relax, and let the Voice Plugin do its thing!
+
+## Usage
+
+1. Download and install the Voice Plugin for Obsidian.
+2. Enable the Voice Plugin.
+3. Open the Voice plugin settings and enter your personal AWS credentials and region.
+4. Important: Close Obsidian and restart Obsidian.
+5. Enjoy listening to your written content being read aloud.
+
+> **Note**: No AWS Account? No problem! Get a free account on [aws.amazon.com](https://aws.amazon.com/) to get started with the Free Tier. With the Free Tier, you'll have access to 1 million characters per month for Neural voices or Speech Marks requests for the first 12 months, starting from your initial speech request.
+
+> **Note**: The Voice Plugin is currently in alpha, and your feedback is greatly appreciated. As an alpha version, it may have limitations and bugs. Your insights and suggestions will help shape the future development of the plugin, ensuring its continued improvement and refinement.
---
+++ src/PollyService.ts
@@ -0,0 +1,214 @@
+import { PollyClient, SynthesizeSpeechCommand } from "@aws-sdk/client-polly";
+
+interface AwsCredentials {
+	credentials: {
+		accessKeyId: string;
+		secretAccessKey: string;
+	};
+	region: string;
+}
+
+interface SynthesizeInput {
+	Engine?: string;
+	LanguageCode?: string;
+	SampleRate?: string;
+	TextType?: string;
+	OutputFormat?: string;
+	VoiceId?: string;
+	Text: string;
+}
+
+export class PollyService {
+	private audio: HTMLAudioElement;
+	private synthesizeInput: {
+		Engine: string | "neural";
+		LanguageCode: string | "en-US";
+		SampleRate: string | "24000";
+		TextType: string | "text";
+		OutputFormat: string | "mp3";
+		VoiceId: string | "Joanna";
+		Text: string;
+	};
+	private pollyClient: PollyClient;
+	private voiceChanged: boolean;
+
+	constructor(awsConfig: AwsCredentials, voice: string) {
+		this.audio = new Audio();
+		this.audio.src = "";
+		this.voiceChanged = true;
+		this.synthesizeInput = {
+			Engine: "neural",
+			LanguageCode: "en-US" || "en-GB",
+			SampleRate: "24000",
+			TextType: "text",
+			OutputFormat: "mp3",
+			VoiceId: voice || "Joanna",
+			Text: "No document selected.",
+		};
+		this.pollyClient = new PollyClient({
+			credentials: {
+				accessKeyId: awsConfig.credentials.accessKeyId,
+				secretAccessKey: awsConfig.credentials.secretAccessKey,
+			},
+			region: awsConfig.region,
+		});
+	}
+
+	smartPolly(text: string) {
+		if (text === this.synthesizeInput.Text && !this.voiceChanged) {
+			console.log("💿 Cache Audio");
+			this.playAudio();
+		} else {
+			console.log("🚀 New Audio");
+			this.synthesizeInput.Text = text;
+			this.voiceChanged = false;
+
+			this.callPolly();
+		}
+	}
+
+	async callPolly() {
+		const chunkedTexts = this.chunkText(this.synthesizeInput.Text, 100);
+		const audioChunks: Blob[] = [];
+
+		for (const chunk of chunkedTexts) {
+			const input = {
+				Engine: this.synthesizeInput.Engine,
+				LanguageCode: this.synthesizeInput.LanguageCode,
+				SampleRate: this.synthesizeInput.SampleRate,
+				TextType: this.synthesizeInput.TextType,
+				OutputFormat: this.synthesizeInput.OutputFormat,
+				Text: chunk,
+				VoiceId: this.synthesizeInput.VoiceId,
+			};
+
+			const command = new SynthesizeSpeechCommand(input);
+
+			try {
+				const data = await this.pollyClient.send(command);
+				if (!data || !data.AudioStream) {
+					throw new Error("Invalid response from Polly");
+				}
+
+				const audioBlob = await this.convertToBlob(data.AudioStream);
+				audioChunks.push(audioBlob);
+			} catch (error) {
+				console.error("Error playing the audio stream:", error);
+				return;
+			}
+		}
+
+		if (audioChunks.length > 0) {
+			const concatenatedAudioBlob = new Blob(audioChunks, {
+				type: "audio/mp3",
+			});
+			this.audio.src = URL.createObjectURL(concatenatedAudioBlob);
+			this.playAudio();
+		}
+	}
+
+	async playAudio() {
+		this.audio.play();
+	}
+
+	pauseAudio() {
+		this.audio.pause();
+	}
+
+	stopAudio() {
+		this.audio.pause();
+		this.audio.currentTime = 0;
+	}
+
+	setSynthesizeInput(synthesizeInput: SynthesizeInput) {
+		this.synthesizeInput = {
+			...this.synthesizeInput,
+			...synthesizeInput,
+		};
+	}
+
+	setAudio(text: string) {
+		this.synthesizeInput.Text = text;
+	}
+
+	setVoice(voice: string) {
+		this.synthesizeInput.VoiceId = voice;
+		this.voiceChanged = true;
+	}
+
+	isPlaying() {
+		return !this.audio.paused;
+	}
+
+	hasEnded() {
+		return this.audio.ended;
+	}
+
+	getContent() {
+		return this.synthesizeInput.Text;
+	}
+
+	getVoice() {
+		return this.synthesizeInput.VoiceId;
+	}
+
+	getAudio() {
+		return this.audio;
+	}
+
+	getDuration() {
+		return this.audio.duration;
+	}
+
+	getCurrentTime() {
+		return this.audio.currentTime;
+	}
+
+	getVolume() {
+		return this.audio.volume;
+	}
+
+	private chunkText(text: string, chunkSize: number): string[] {
+		const words: string[] = text.split(" ");
+		const chunks: string[] = [];
+		let startIndex = 0;
+
+		while (startIndex < words.length) {
+			const chunk = words
+				.slice(startIndex, startIndex + chunkSize)
+				.join(" ");
+			chunks.push(chunk);
+			startIndex += chunkSize;
+		}
+
+		return chunks;
+	}
+
+	private async convertToBlob(audioStream: ReadableStream<Uint8Array>) {
+		const reader = audioStream.getReader();
+		const blobParts: Uint8Array[] = [];
+		let totalLength = 0;
+
+		while (true) {
+			const { done, value } = await reader.read();
+			if (done) break;
+
+			blobParts.push(value);
+			totalLength += value.length;
+		}
+
+		const audioBlob = new Blob(blobParts, {
+			type: "audio/mp3",
+		});
+
+		return audioBlob;
+	}
+
+	sleep(num: number): Promise<void> {
+		return new Promise<void>((resolve) => {
+			setTimeout(() => {
+				resolve();
+			}, num);
+		});
+	}
+}

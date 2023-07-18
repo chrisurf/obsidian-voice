2168a5edc23cb37f9f8427898994c92a39d6562e
chris oguntolu <44543411+chrisurf@users.noreply.github.com>
2023-07-18 11:42:13 +0200

    🔉 Updating build, adding  MIT License

--- src/PollyService.ts
+++ src/PollyService.ts
@@ -1,214 +1,217 @@
 import { PollyClient, SynthesizeSpeechCommand } from "@aws-sdk/client-polly";
+import { Readable } from "stream";
 
 interface AwsCredentials {
-	credentials: {
-		accessKeyId: string;
-		secretAccessKey: string;
-	};
-	region: string;
+  credentials: {
+    accessKeyId: string;
+    secretAccessKey: string;
+  };
+  region: string;
 }
 
 interface SynthesizeInput {
-	Engine?: string;
-	LanguageCode?: string;
-	SampleRate?: string;
-	TextType?: string;
-	OutputFormat?: string;
-	VoiceId?: string;
-	Text: string;
+  Engine?: string;
+  LanguageCode?: string;
+  SampleRate?: string;
+  TextType?: string;
+  OutputFormat?: string;
+  VoiceId?: string;
+  Text: string;
 }
 
 export class PollyService {
-	private audio: HTMLAudioElement;
-	private synthesizeInput: {
-		Engine: string | "neural";
-		LanguageCode: string | "en-US";
-		SampleRate: string | "24000";
-		TextType: string | "text";
-		OutputFormat: string | "mp3";
-		VoiceId: string | "Joanna";
-		Text: string;
-	};
-	private pollyClient: PollyClient;
-	private voiceChanged: boolean;
+  private audio: HTMLAudioElement;
+  private synthesizeInput: {
+    Engine: string | "neural";
+    LanguageCode: string | "en-US";
+    SampleRate: string | "24000";
+    TextType: string | "text";
+    OutputFormat: string | "mp3";
+    VoiceId: string | "Joanna";
+    Text: string;
+  };
+  private pollyClient: PollyClient;
+  private voiceChanged: boolean;
 
-	constructor(awsConfig: AwsCredentials, voice: string) {
-		this.audio = new Audio();
-		this.audio.src = "";
-		this.voiceChanged = true;
-		this.synthesizeInput = {
-			Engine: "neural",
-			LanguageCode: "en-US" || "en-GB",
-			SampleRate: "24000",
-			TextType: "text",
-			OutputFormat: "mp3",
-			VoiceId: voice || "Joanna",
-			Text: "No document selected.",
-		};
-		this.pollyClient = new PollyClient({
-			credentials: {
-				accessKeyId: awsConfig.credentials.accessKeyId,
-				secretAccessKey: awsConfig.credentials.secretAccessKey,
-			},
-			region: awsConfig.region,
-		});
-	}
+  constructor(awsConfig: AwsCredentials, voice: string) {
+    this.audio = new Audio();
+    this.audio.src = "";
+    this.voiceChanged = true;
+    this.synthesizeInput = {
+      Engine: "neural",
+      LanguageCode: "en-US" || "en-GB",
+      SampleRate: "24000",
+      TextType: "text",
+      OutputFormat: "mp3",
+      VoiceId: voice || "Joanna",
+      Text: "No document selected.",
+    };
+    this.pollyClient = new PollyClient({
+      credentials: {
+        accessKeyId: awsConfig.credentials.accessKeyId,
+        secretAccessKey: awsConfig.credentials.secretAccessKey,
+      },
+      region: awsConfig.region,
+    });
+  }
 
-	smartPolly(text: string) {
-		if (text === this.synthesizeInput.Text && !this.voiceChanged) {
-			console.log("💿 Cache Audio");
-			this.playAudio();
-		} else {
-			console.log("🚀 New Audio");
-			this.synthesizeInput.Text = text;
-			this.voiceChanged = false;
+  smartPolly(text: string) {
+    if (text === this.synthesizeInput.Text && !this.voiceChanged) {
+      console.log("💿 Cache Audio");
+      this.playAudio();
+    } else {
+      console.log("🚀 New Audio");
+      this.synthesizeInput.Text = text;
+      this.voiceChanged = false;
 
-			this.callPolly();
-		}
-	}
+      this.callPolly();
+    }
+  }
 
-	async callPolly() {
-		const chunkedTexts = this.chunkText(this.synthesizeInput.Text, 100);
-		const audioChunks: Blob[] = [];
+  async callPolly() {
+    const chunkedTexts = this.chunkText(this.synthesizeInput.Text, 100);
+    const audioChunks: Blob[] = [];
 
-		for (const chunk of chunkedTexts) {
-			const input = {
-				Engine: this.synthesizeInput.Engine,
-				LanguageCode: this.synthesizeInput.LanguageCode,
-				SampleRate: this.synthesizeInput.SampleRate,
-				TextType: this.synthesizeInput.TextType,
-				OutputFormat: this.synthesizeInput.OutputFormat,
-				Text: chunk,
-				VoiceId: this.synthesizeInput.VoiceId,
-			};
+    for (const chunk of chunkedTexts) {
+      const input = {
+        Engine: this.synthesizeInput.Engine,
+        LanguageCode: this.synthesizeInput.LanguageCode,
+        SampleRate: this.synthesizeInput.SampleRate,
+        TextType: this.synthesizeInput.TextType,
+        OutputFormat: this.synthesizeInput.OutputFormat,
+        Text: chunk,
+        VoiceId: this.synthesizeInput.VoiceId,
+      };
 
-			const command = new SynthesizeSpeechCommand(input);
+      const command = new SynthesizeSpeechCommand(input);
 
-			try {
-				const data = await this.pollyClient.send(command);
-				if (!data || !data.AudioStream) {
-					throw new Error("Invalid response from Polly");
-				}
+      try {
+        const data = await this.pollyClient.send(command);
+        if (!data || !data.AudioStream) {
+          throw new Error("Invalid response from Polly");
+        }
 
-				const audioBlob = await this.convertToBlob(data.AudioStream);
-				audioChunks.push(audioBlob);
-			} catch (error) {
-				console.error("Error playing the audio stream:", error);
-				return;
-			}
-		}
+        const audioBlob = await this.convertToBlob(data.AudioStream);
+        audioChunks.push(audioBlob);
+      } catch (error) {
+        console.error("Error playing the audio stream:", error);
+        return;
+      }
+    }
 
-		if (audioChunks.length > 0) {
-			const concatenatedAudioBlob = new Blob(audioChunks, {
-				type: "audio/mp3",
-			});
-			this.audio.src = URL.createObjectURL(concatenatedAudioBlob);
-			this.playAudio();
-		}
-	}
+    if (audioChunks.length > 0) {
+      const concatenatedAudioBlob = new Blob(audioChunks, {
+        type: "audio/mp3",
+      });
+      this.audio.src = URL.createObjectURL(concatenatedAudioBlob);
+      this.playAudio();
+    }
+  }
 
-	async playAudio() {
-		this.audio.play();
-	}
+  async playAudio() {
+    this.audio.play();
+  }
 
-	pauseAudio() {
-		this.audio.pause();
-	}
+  pauseAudio() {
+    this.audio.pause();
+  }
 
-	stopAudio() {
-		this.audio.pause();
-		this.audio.currentTime = 0;
-	}
+  stopAudio() {
+    this.audio.pause();
+    this.audio.currentTime = 0;
+  }
 
-	setSynthesizeInput(synthesizeInput: SynthesizeInput) {
-		this.synthesizeInput = {
-			...this.synthesizeInput,
-			...synthesizeInput,
-		};
-	}
+  setSynthesizeInput(synthesizeInput: SynthesizeInput) {
+    this.synthesizeInput = {
+      ...this.synthesizeInput,
+      ...synthesizeInput,
+    };
+  }
 
-	setAudio(text: string) {
-		this.synthesizeInput.Text = text;
-	}
+  setAudio(text: string) {
+    this.synthesizeInput.Text = text;
+  }
 
-	setVoice(voice: string) {
-		this.synthesizeInput.VoiceId = voice;
-		this.voiceChanged = true;
-	}
+  setVoice(voice: string) {
+    this.synthesizeInput.VoiceId = voice;
+    this.voiceChanged = true;
+  }
 
-	isPlaying() {
-		return !this.audio.paused;
-	}
+  isPlaying() {
+    return !this.audio.paused;
+  }
 
-	hasEnded() {
-		return this.audio.ended;
-	}
+  hasEnded() {
+    return this.audio.ended;
+  }
 
-	getContent() {
-		return this.synthesizeInput.Text;
-	}
+  getContent() {
+    return this.synthesizeInput.Text;
+  }
 
-	getVoice() {
-		return this.synthesizeInput.VoiceId;
-	}
+  getVoice() {
+    return this.synthesizeInput.VoiceId;
+  }
 
-	getAudio() {
-		return this.audio;
-	}
+  getAudio() {
+    return this.audio;
+  }
 
-	getDuration() {
-		return this.audio.duration;
-	}
+  getDuration() {
+    return this.audio.duration;
+  }
 
-	getCurrentTime() {
-		return this.audio.currentTime;
-	}
+  getCurrentTime() {
+    return this.audio.currentTime;
+  }
 
-	getVolume() {
-		return this.audio.volume;
-	}
+  getVolume() {
+    return this.audio.volume;
+  }
 
-	private chunkText(text: string, chunkSize: number): string[] {
-		const words: string[] = text.split(" ");
-		const chunks: string[] = [];
-		let startIndex = 0;
+  private chunkText(text: string, chunkSize: number): string[] {
+    const words: string[] = text.split(" ");
+    const chunks: string[] = [];
+    let startIndex = 0;
 
-		while (startIndex < words.length) {
-			const chunk = words
-				.slice(startIndex, startIndex + chunkSize)
-				.join(" ");
-			chunks.push(chunk);
-			startIndex += chunkSize;
-		}
+    while (startIndex < words.length) {
+      const chunk = words.slice(startIndex, startIndex + chunkSize).join(" ");
+      chunks.push(chunk);
+      startIndex += chunkSize;
+    }
 
-		return chunks;
-	}
+    return chunks;
+  }
 
-	private async convertToBlob(audioStream: ReadableStream<Uint8Array>) {
-		const reader = audioStream.getReader();
-		const blobParts: Uint8Array[] = [];
-		let totalLength = 0;
+  private async convertToBlob(
+    audioStream: Blob | Readable | ReadableStream<Uint8Array>
+  ) {
+    const readableStream = audioStream as ReadableStream<Uint8Array>;
 
-		while (true) {
-			const { done, value } = await reader.read();
-			if (done) break;
+    const reader = readableStream.getReader();
+    const blobParts: Uint8Array[] = [];
+    let totalLength = 0;
 
-			blobParts.push(value);
-			totalLength += value.length;
-		}
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) break;
 
-		const audioBlob = new Blob(blobParts, {
-			type: "audio/mp3",
-		});
+      blobParts.push(value);
+      totalLength += value.length;
+    }
 
-		return audioBlob;
-	}
+    const audioBlob = new Blob(blobParts, {
+      type: "audio/mp3",
+    });
 
-	sleep(num: number): Promise<void> {
-		return new Promise<void>((resolve) => {
-			setTimeout(() => {
-				resolve();
-			}, num);
-		});
-	}
+    return audioBlob;
+  }
+
+  sleep(num: number): Promise<void> {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, num);
+    });
+  }
 }

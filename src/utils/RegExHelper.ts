229be694eafc3a536ae07ba8ab35770e0290d61b
chrisurf <me@chrisurf.com>
2025-06-07 18:13:29 +0200

    fix: emojis bug

--- src/utils/RegExHelper.ts
+++ src/utils/RegExHelper.ts
@@ -32,11 +32,22 @@ export class RegExHelper {
     this.content = result;
   }
 
+  removeEmojis() {
+    const text = this.content;
+    // This regex matches most emoji characters
+    var result: string = text.replace(
+      /[\u{1F000}-\u{1FFFF}]|\p{Emoji_Presentation}|\p{Extended_Pictographic}/gu,
+      "",
+    );
+    this.content = result;
+  }
+
   getcleanContent() {
     this.removeHeader();
     this.removeCode();
     this.removeLinks();
     this.removeSpecialCharacters();
+    this.removeEmojis(); // Add the new emoji removal step
     return this.content;
   }
 }
